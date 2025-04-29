914e022fffcc189edbdfe0238e05815cce54bdd0
Uliana Degtiareva <uvdegtiareva@edu.hse.ru>
2025-04-29 17:14:36 +0500

    правки в cats module

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -3,9 +3,12 @@ import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 import { CatEntity } from './entities/cat.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CatEntity])],
+  imports: [
+    TypeOrmModule.forFeature([CatEntity, UserEntity])
+  ],
   controllers: [CatsController],
   providers: [CatsService],
 })
