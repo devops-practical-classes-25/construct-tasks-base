0ca1e7c2ced0194438174494bfc131006eccd5eb
Uliana Degtiareva <uvdegtiareva@edu.hse.ru>
2025-05-04 17:31:01 +0500

    правки в main

--- src/cats/schemas/cat.schema.ts
+++
@@ -1,7 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { CreateCatSchema } from "./create-cat.schema";
-
-export class CatSchema extends CreateCatSchema {
-  @ApiProperty({ example: 1, description: 'The unique identifier of the cat' })
-  id: number;
-}
